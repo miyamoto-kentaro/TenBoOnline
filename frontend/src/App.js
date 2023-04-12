eb873886d88dfe5246c6dd42339a2bce33c64b7f
miyamoto-kentaro <57547673+miyamoto-kentaro@users.noreply.github.com>
2023-04-12 21:37:38 +0900

    com

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GamePanel from "./components/GamePanel";
 import HomeScreen from "./screens/HomeScreen";
 function App() {
+  console.log(process.env.REACT_APP_ROOT_URL);
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<GamePanel />} />
           <Route
-            path={process.env.REACT_APP_HELLO_WORLD + "page1"}
+            path={process.env.REACT_APP_ROOT_URL + "page1"}
             element={<HomeScreen />}
           />
           <Route
-            path={process.env.REACT_APP_HELLO_WORLD + "page2"}
+            path={process.env.REACT_APP_ROOT_URL + "page2"}
             element={<GamePanel />}
           />
         </Routes>
