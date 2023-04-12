dc0faf1f03ca259a7ba39ad6e77b45b78e52788e
miyamoto-kentaro <57547673+miyamoto-kentaro@users.noreply.github.com>
2023-04-12 21:49:35 +0900

    com

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
 export default function HomeScreen() {
-  return <h1>aa</h1>;
+  return (
+    <>
+      <h1>aa</h1>
+
+      <Link to="page2">Sample Page2</Link>
+    </>
+  );
 }
